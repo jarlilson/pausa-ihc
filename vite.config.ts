c7d2f8ded942914e32ecfddfa1349ef9aac9abab
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2025-10-26 19:21:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages base path will be injected by Actions as VITE_BASE="/<repo>/"
-  base: process.env.VITE_BASE ?? '/',
+  base: '/pausa-ihc/', // <= NOME EXATO DO REPO, com barras
 })
