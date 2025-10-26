1ba389a89326a73e3e79b417d1803b1d0861577b
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2025-10-26 19:12:54 -0300

    Delete src/ui/ScrollArea.tsx

--- src/ui/ScrollArea.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function ScrollArea({ className, children }: { className?: string; children: ReactNode }) {
-  return <div className={`overflow-y-auto ${className ?? ""}`}>{children}</div>;
-}
